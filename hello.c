9cf083cd47dd0d6687d8c6c07051505248c04a67
Raymond Biko <raybiko@gmail.com>
2023-03-31 22:04:20 +0300

    fisrtc

---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Me coding \n");//hey comment
+    printf("Me \n nag");//hey comment
+    printf("Me \n n\ta\tg");//hey comment
+    /*
+    
+    this is a multi line comment
+    
+    */
+    return 0;
+}
